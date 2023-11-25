a2bbfcf8f4773214e8b5716dde81e4b54615b6b7
ashrafahmedthabet <asthabet@noon.com>
2023-11-25 17:44:12 +0200

    created portfolio

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// typedjs
+var typed = new Typed("#element", {
+  strings: ["Larry Daniels", "Developer", "Designer"],
+  typeSpeed: 55,
+  backSpeed: 55,
+  loop: true,
+  loopCount: Infinity,
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  nav: false,
+  items: 1,
+});
